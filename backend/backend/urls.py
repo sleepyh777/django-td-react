ae25e4dea0208eeace90404e40b0e3a0b3138c28
ankiyang <ankiyang1201@gmail.com>
2019-12-26 19:45:11 +1300

    django init

---
+++ backend/backend/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.contrib import admin
+from rest_framework import routers
+from todo import views
+
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
+]
