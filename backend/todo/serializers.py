af22db0cc57da995408fb72d66254200819b7896
ankiyang <ankiyang1201@gmail.com>
2019-12-26 22:22:11 +1300

    fix: dj version up; corsheaders new v

--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -1,8 +1,10 @@
+#!/usr/local/bin/python3.6
+# -*- coding: utf-8 -*-
+
 from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'completed')
